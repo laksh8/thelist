0d64525ef1850a05934e6383b825f651913db58b
Laksh <laksh8396@gmail.com>
2025-03-20 14:02:09 +0530

    initial commit

---
+++ lib/Activities/canvas.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CanvasActivity extends StatelessWidget {
+  const CanvasActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
